2c8aa6a7d59f0def593bce1f385f4b34f88b9231
Chris Kral <christoph3r.kral@gmail.com>
2022-01-07 19:39:49 +0000

    game result commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+    computerMove = 'papier';
+} else if (randomNumber == 3){
+    computerMove = 'nozyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -13,3 +31,25 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Computer wygrywa!');
+} else if (computerMove == 'nozyce' && playerMove == 'nozyce'){
+    printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+} else if (computerMove == 'nozyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nozyce' && playerMove == 'papier'){
+    printMessage('Computer wygrywa!');
+} else if (computerMove == 'papier' && playerMove == 'nozyce'){
+    printMessage('Computer wygrywa!');
+} else if (computerMove == 'kamień' && playerMove == 'nozyce'){
+    printMessage('Ty wygrywasz!');
+} else {
+    printMessage('Przegrywasz walkowerem!')
+}
