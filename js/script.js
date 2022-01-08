1b7befc9e81464b6b97e5d066d3297fdb857a085
Chris Kral <christoph3r.kral@gmail.com>
2022-01-08 21:58:50 +0000

    Implementation of logic through JS function

--- js/script.js
+++ js/script.js
@@ -1,55 +1,78 @@
+function getMoveName(argMoveId){
+   
+    if(argMoveId == 1){
+        return 'kamień';
+    } else if (argMoveId == 2){
+        return 'papier';
+    } else if (argMoveId == 3){
+        return 'nozyce';
+    } else {
+      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+      return 'nieznany ruch';
+    }
+  }
+
+function displayResult(argComputerMove, argPlayerMove){
+  
+     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+     if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+      } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+        printMessage('Computer wygrywa!');
+    } else if (argComputerMove == 'nozyce' && argPlayerMove == 'nozyce'){
+        printMessage('Remis!');
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'papier'){
+        printMessage('Remis!');
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+        printMessage('Remis!');
+    } else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+    } else if (argComputerMove == 'nozyce' && argPlayerMove == 'papier'){
+        printMessage('Computer wygrywa!');
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce'){
+        printMessage('Computer wygrywa!');
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nozyce'){
+        printMessage('Ty wygrywasz!');
+    } else {
+        printMessage('Przegrywasz walkowerem!');
+    }
+  }
+
+
+  
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let argComputerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if (randomNumber == 2){
-    computerMove = 'papier';
-} else if (randomNumber == 3){
-    computerMove = 'nozyce';
-}
+// if(randomNumber == 1){
+//   computerMove = 'kamień';
+// } else if (randomNumber == 2){
+//     computerMove = 'papier';
+// } else if (randomNumber == 3){
+//     computerMove = 'nozyce';
+// }
 
-printMessage('Mój ruch to: ' + computerMove);
-
-
+printMessage('Mój ruch to: ' + argComputerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let argPlayerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (playerInput =='2'){
-    playerMove = 'papier';
-} else if (playerInput =='3'){
-    playerMove = 'nozyce';
-}
+// if(playerInput == '1'){
+//   playerMove = 'kamień';
+// } else if (playerInput =='2'){
+//     playerMove = 'papier';
+// } else if (playerInput =='3'){
+//     playerMove = 'nozyce';
+// }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + argPlayerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Computer wygrywa!');
-} else if (computerMove == 'nozyce' && playerMove == 'nozyce'){
-    printMessage('Remis!');
-} else if (computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis!');
-} else if (computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
-} else if (computerMove == 'nozyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-} else if (computerMove == 'nozyce' && playerMove == 'papier'){
-    printMessage('Computer wygrywa!');
-} else if (computerMove == 'papier' && playerMove == 'nozyce'){
-    printMessage('Computer wygrywa!');
-} else if (computerMove == 'kamień' && playerMove == 'nozyce'){
-    printMessage('Ty wygrywasz!');
-} else {
-    printMessage('Przegrywasz walkowerem!')
-}
+displayResult(argComputerMove, argPlayerMove);
+
+
+
