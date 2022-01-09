96c04de276d543661fbf16edad6b9372f5221c0a
Chris Kral <christoph3r.kral@gmail.com>
2022-01-09 20:45:49 +0000

    styling fix, adding function clearMessage()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function playGame(playerInput) {
+    clearMessages();
     function getMoveName(argMoveId) {
         if (argMoveId == 1) {
           return 'kamień';
@@ -72,14 +73,14 @@ function playGame(playerInput) {
   displayResult(argComputerMove, argPlayerMove);
 } 
 
-document.getElementById('play-rock').addEventListener('click', function(){
+document.getElementById('play-rock').addEventListener('click', function() {
     playGame(1);
   });
 
-document.getElementById('play-paper').addEventListener('click', function(){
+document.getElementById('play-paper').addEventListener('click', function() {
     playGame(2);
   });
   
-document.getElementById('play-scissors').addEventListener('click', function(){
+document.getElementById('play-scissors').addEventListener('click', function() {
     playGame(3);
   });
